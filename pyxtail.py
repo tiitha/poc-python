775c9b44eb3296fdbf9f12807169a6ac6c82e733
Tiit Hallas <tiit.hallas@smit.ee>
2017-08-28 17:26:28 +0300

    pyxtailer

--- pyxtail.py
+++ pyxtail.py
@@ -10,17 +10,17 @@ f2bf = {				# files to be followed
 
 
 def scan_folder():
-    # check for existing files in the queue
-    tmp_f2bf = dict(f2bf)
-    for f in tmp_f2bf:
-        if not os.path.isfile(f):
-            # File-not-Found: remove file from queue
-            del f2bf[f]
+	# check for existing files in the queue
+	tmp_f2bf = dict(f2bf)
+	for f in tmp_f2bf:
+		if not os.path.isfile(f):
+			# File-not-Found: remove file from queue
+			del f2bf[f]
 
-    # searching for the new files in the root path
-    for ff in glob.glob(file_pattern):
-        if ff not in f2bf:
-            f2bf[ff] = 0
+	# searching for the new files in the root path
+	for ff in glob.glob(file_pattern):
+		if ff not in f2bf:
+			f2bf[ff] = 0
 
 
 def follow():
@@ -31,21 +31,21 @@ def follow():
 			pointer = f2bf[f]
 			try:
 				with open(f, "r") as fo:
-	                if pointer == 0:
-	                    fo.seek(0,2)
-	                    f2bf[f] = fo.tell()
-	                else:
-                        fo.seek(pointer)
+					if pointer == 0:
+						fo.seek(0,2)
+						f2bf[f] = fo.tell()
+					else:
+						fo.seek(pointer)
 
-	                buf = fo.read(buffer_size)
-	                i = buf.rfind(line_break)
-	                if i == -1:
-                        continue;
+					buf = fo.read(buffer_size)
+					i = buf.rfind(line_break)
+					if i == -1:
+						continue;
 
-	                events = buf[:i].split(line_break)
-	                f2bf[f] += i + len(line_break)
-	                for evt in events:
-                        parse_event( evt )
+					events = buf[:i].split(line_break)
+					f2bf[f] += i + len(line_break)
+					for evt in events:
+						parse_event( evt )
 
 			except:
 				pass
